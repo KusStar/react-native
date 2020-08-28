bf4fd9f9fa891ae4fad5fdd0bcfed976513aaa7f
Kuss <2360314753@qq.com>
2020-08-28 17:34:21 +0800

    :lipstick: update: make Modal fullscreen

--- ReactAndroid/src/main/java/com/facebook/react/views/modal/ReactModalHostView.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/modal/ReactModalHostView.java
@@ -264,7 +264,11 @@ public class ReactModalHostView extends ViewGroup implements LifecycleEventListe
   private View getContentView() {
     FrameLayout frameLayout = new FrameLayout(getContext());
     frameLayout.addView(mHostView);
-    frameLayout.setFitsSystemWindows(true);
+    frameLayout.setSystemUiVisibility(
+      View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    );
     return frameLayout;
   }
 
