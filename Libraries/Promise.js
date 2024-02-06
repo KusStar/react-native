7f19f7d39231f8be52e3e5460af499591f01759f
kusstar <kussssss@outlook.com>
2024-02-07 01:39:54 +0800

    feat: Promise symbolicateStackTrace Promise onUnahandled warn

--- Libraries/Promise.js
+++ Libraries/Promise.js
@@ -15,7 +15,7 @@
  * run Flow. */
 const Promise = require('fbjs/lib/Promise.native');
 
-if (__DEV__) {
+if (__DEV__ || global._isDebug) {
   /* $FlowFixMe(>=0.54.0 site=react_native_oss) This comment suppresses an
    * error found when Flow v0.54 was deployed. To see the error delete this
    * comment and run Flow. */
@@ -23,12 +23,10 @@ if (__DEV__) {
     allRejections: true,
     onUnhandled: (id, error = {}) => {
       let message: string;
-      let stack: ?string;
 
       const stringValue = Object.prototype.toString.call(error);
       if (stringValue === '[object Error]') {
         message = Error.prototype.toString.call(error);
-        stack = error.stack;
       } else {
         /* $FlowFixMe(>=0.54.0 site=react_native_oss) This comment suppresses
          * an error found when Flow v0.54 was deployed. To see the error delete
@@ -36,11 +34,48 @@ if (__DEV__) {
         message = require('pretty-format')(error);
       }
 
-      const warning =
-        `Possible Unhandled Promise Rejection (id: ${id}):\n` +
-        `${message}\n` +
-        (stack == null ? '' : stack);
-      console.warn(warning);
+      const parseErrorStack = require('parseErrorStack');
+      const stack = parseErrorStack(error);
+
+      if (!stack) {
+        const warning =
+          `Possible Unhandled Promise Rejection (id: ${id}):\n` +
+          `${message}\n`
+        console.warn(warning);
+        return
+      }
+
+      const symbolicateStackTrace = require('symbolicateStackTrace');
+      symbolicateStackTrace(stack)
+        .then(prettyStack => {
+          if (prettyStack) {
+            const stackLines = prettyStack
+              .map(stack => {
+                const file = stack.file || '';
+                const lineNumber = stack.lineNumber || '';
+                const column = stack.column || '';
+                const sep1 = file && lineNumber ? ':' : ''
+                const sep2 = lineNumber && column ? ':' : ''
+                const extra = `${file}${sep1}${lineNumber}${sep2}${column}`
+                return `at ${stack.methodName} ${extra ? ' (' + extra + ')' : ''}`
+              })
+              .join('\n\t')
+            const warning =
+              `Possible Unhandled Promise Rejection (id: ${id}):\n    ` +
+              `${message}\n\t` +
+              (stackLines);
+            console.warn(warning);
+          } else {
+            const warning =
+              `Possible Unhandled Promise Rejection (id: ${id}):\n` +
+              `${message}\n` +
+              (error.stack == null ? '' : error.stack);
+            console.warn(warning);
+          }
+        })
+        .catch((e) => {
+          throw e;
+        })
     },
     onHandled: id => {
       const warning =
